c031e5f929741d9de49c44c2463b1ae504a3d366
AdamSimpson1317 <adam.simpson.13@hotmail.com>
2023-07-08 17:02:00 +0100

    Patroller Basic Movement

--- gmtk-project/Assets/Scripts/Patroler.cs
+++ gmtk-project/Assets/Scripts/Patroler.cs
@@ -4,8 +4,29 @@ using UnityEngine;
 
 public class Patroler : Unit
 {
+
+    public float speed = 0.8f;
+    public float range = 3f;
+
+    float startingY;
+    int dir = 1;
+
     public void Patrol()
     {
         //Patrol area
+    }
+
+    void start()
+    {
+        startingY = transform.position.y;
+    }
+
+    void FixedUpdate()
+    {
+        transform.Translate(Vector2.up * speed * Time.deltaTime * dir);
+        if(transform.position.y < startingY || transform.position.y > startingY + range)
+        {
+            dir *= -1;
+        }
     }
 }
